e6a6160e5974b2f83306345edcfc572049ef5357
Hitesh Chawda <hiteshchawda105@gmail.com>
2026-09-24 22:09:44 +0530

    case-sensitive error fix

--- Backend/src/routes/Pyq.routes.js
+++ Backend/src/routes/Pyq.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT, verifyCreator } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.js";
-import {uploadPyq, getAllPyqs, deletePyq} from "../controllers/pyq.controllers.js";
+import {uploadPyq, getAllPyqs, deletePyq} from "../controllers/Pyq.controllers.js";
 
 const router = Router();
 
